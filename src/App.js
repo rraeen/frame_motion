2f482dbc983cee2ead675dfcc8090b2f5e8b401a
raunakali_ty <raunakali.r@testyantra.in>
2023-10-08 19:49:24 +0530

    frame_motion

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg'
+import './App.css'
+import ScrollEffect from './Component/ScrollEffect'
+
+function App() {
+
+ 
+
+
+
+
+  return (
+  <ScrollEffect/>
+  )
+}
+
+export default App
